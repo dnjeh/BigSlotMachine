2fc848a8dd4d011b6b11a308107d6c3b28f5b1d9
dnjeh <ehdnj.001@gmail.com>
2023-11-18 23:56:20 +0900

    Today's once-rewrited slot's

--- fixing.h
+++ fixing.h
@@ -5,9 +5,22 @@
 #include <windows.h> //콘솔2
 #include <string.h>  //문자열 관련된 편리한 것들!
 #include <errno.h>   //파일 쓸때 같이 씀
+#include <process.h> //스레드 관련 헤더파일?
 
 static HANDLE bsmBuf[2];
 static int bsmNowBuf;
+static int usedGacha;
+static int usedSlot;
+
+//화면 관련 함수
+void bufChange();
+
+//스레드 관련 함수
+unsigned __stdcall clickthr(void *arg);
+unsigned __stdcall gachaScroll(void *arg);
+unsigned __stdcall gachaUpdate(void *arg);
+unsigned __stdcall slot(void *arg);
+
 void ScreenInit() {
     CONSOLE_CURSOR_INFO cci;
 
